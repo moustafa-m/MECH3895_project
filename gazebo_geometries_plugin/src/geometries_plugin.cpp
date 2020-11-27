2dabb4dd2961c2c6ad74b7500fa34fe7f6b9652e
Moustafa <mustafa.motawei@gmail.com>
2020-11-27 01:20:47 +0000

    add capability to get link collisions

--- gazebo_geometries_plugin/src/geometries_plugin.cpp
+++ gazebo_geometries_plugin/src/geometries_plugin.cpp
@@ -27,32 +27,102 @@ void GeometriesPlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
 
 bool GeometriesPlugin::getGeometrySrv(gazebo_geometries_plugin::geometry::Request &req, gazebo_geometries_plugin::geometry::Response &res)
 {
-    physics::ModelPtr model = world_->ModelByName(req.model_name);
+    physics::ModelPtr model;
+    bool find_link = false;
 
+    // first check if user is asking for a link's geometry
+    std::string::size_type pos = req.model_name.find("_link");
+    if (pos != std::string::npos)
+    {
+        find_link = true;
+
+        ROS_INFO("%sGeometriesPlugin: Requests for link [%s] recieved!", GREEN, req.model_name.c_str());
+        std::string name = req.model_name.substr(0, pos);
+        ROS_INFO("%sGeometriesPlugin: Looking for parent [%s]", GREEN, name.c_str());
+        model = world_->ModelByName(name);
+    }
+    else
+    {
+        model = world_->ModelByName(req.model_name);
+        ROS_INFO("%sGeometriesPlugin: Looking for [%s]", GREEN, req.model_name.c_str());
+    }
+    
     if (!model)
     {
         ROS_ERROR("GeometriesPlugin: Unable to find [%s]! Model does not exist", req.model_name.c_str());
 
         res.message = "Error, model does not exist!";
         geometry_msgs::Vector3 size;
-        size.x = size.y = size.z = NAN;
-        res.min_bounds = res.max_bounds = size;
+        // size.x = size.y = size.z = NAN;
+        // res.min_bounds = res.max_bounds = size;
         return false;
     }
 
-    ROS_INFO("%sGeometriesPlugin: Found [%s]!", GREEN, req.model_name.c_str());
+    ignition::math::Box collision_box;
+    geometry_msgs::Vector3 min, max, centre, dimensions;
+    ROS_INFO("%sGeometriesPlugin: Found %d child links for [%s]", GREEN, model->GetChildCount(), req.model_name.c_str());
+    
+    // loop through parent model child links
+    for (unsigned int i = 0 ; i < model->GetChildCount(); i ++)
+    {
+        gazebo::physics::LinkPtr body = boost::dynamic_pointer_cast<gazebo::physics::Link>(model->GetChild(i));
 
-    res.message = "Model found!";
+        if (body)
+        {
+            std::size_t npos = std::string::npos;
+            // NOTE: kinova model has multiple child links, each with multiple collision geometry, default gazebo models have one
+            // child link with multiple collision geometries
+            std::string::size_type pos = req.model_name.find("link_");
+            std::string link_name = req.model_name.substr(pos+5, req.model_name.length());
+            if (req.model_name.find("j2s7s300") != npos || req.model_name.find("j2s6s300") != npos || req.model_name.find("j2n6s300") != npos)
+            {
+                link_name = req.model_name + "_collision";
+            }
+            
+            for (unsigned int j = 0; j < body->GetChildCount() ; j++)
+            {
+                // check if user is asking for a specific link
+                if (!find_link)
+                {
+                    gazebo::physics::CollisionPtr geom = boost::dynamic_pointer_cast<gazebo::physics::Collision>(body->GetChild(j));
+                    collision_box = geom->CollisionBoundingBox();
+                    
+                    min.x = collision_box.Min().X(); min.y = collision_box.Min().Y(); min.z = collision_box.Min().Z();
+                    max.x = collision_box.Max().X(); max.y = collision_box.Max().Y(); max.z = collision_box.Max().Z();
+                    centre.x = collision_box.Center().X(); centre.y = collision_box.Center().Y(); centre.z = collision_box.Center().Z();
+                    dimensions.x = collision_box.XLength(); dimensions.y = collision_box.YLength(); dimensions.z = collision_box.ZLength();
 
-    ignition::math::Box box = model->CollisionBoundingBox();
+                    res.name.push_back(req.model_name + "_" + body->GetChild(j)->GetName());
+                    res.min_bounds.push_back(min);
+                    res.max_bounds.push_back(max);
+                    res.centre.push_back(centre);
+                    res.dimensions.push_back(dimensions);
+                }
+                else if (body->GetChild(j)->GetName().compare(link_name) == 0)
+                {
+                    ROS_INFO("%sGeometriesPlugin: Found [%s]!", GREEN, link_name.c_str());
+                    gazebo::physics::CollisionPtr geom = boost::dynamic_pointer_cast<gazebo::physics::Collision>(body->GetChild(j));
+                    collision_box = geom->CollisionBoundingBox();
 
-    geometry_msgs::Vector3 min, max, centre;
+                    min.x = collision_box.Min().X(); min.y = collision_box.Min().Y(); min.z = collision_box.Min().Z();
+                    max.x = collision_box.Max().X(); max.y = collision_box.Max().Y(); max.z = collision_box.Max().Z();
+                    centre.x = collision_box.Center().X(); centre.y = collision_box.Center().Y(); centre.z = collision_box.Center().Z();
+                    dimensions.x = collision_box.XLength(); dimensions.y = collision_box.YLength(); dimensions.z = collision_box.ZLength();
 
-    min.x = box.Min().X(); min.y = box.Min().Y(); min.z = box.Min().Z();
-    max.x = box.Max().X(); max.y = box.Max().Y(); max.z = box.Max().Z();
-    centre.x = box.Center().X(); centre.y = box.Center().Y(); centre.z = box.Center().Z();
-
-    res.min_bounds = min; res.max_bounds = max; res.centre = centre;
+                    res.name.push_back(req.model_name);
+                    res.min_bounds.push_back(min);
+                    res.max_bounds.push_back(max);
+                    res.centre.push_back(centre);
+                    res.dimensions.push_back(dimensions);
+                    break;
+                }
+                
+            }
+        }
+    }
+    
+    res.message = "GeomtriesPlugin: Model found!";
+    ROS_INFO("%sGeometriesPlugin: Obtained collision geometries for [%s]", GREEN, req.model_name.c_str());
 
     return true;
 }
