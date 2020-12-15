17d293905a7e0d48c8385067b094cd7ad8268e8e
Moustafa <mustafa.motawei@gmail.com>
2020-12-15 21:15:22 +0000

    implement visualisation in geometries plugin

--- gazebo_geometries_plugin/src/geometries_plugin.cpp
+++ gazebo_geometries_plugin/src/geometries_plugin.cpp
@@ -5,7 +5,7 @@ using namespace gazebo;
 GeometriesPlugin::GeometriesPlugin()
     : WorldPlugin()
 {
-    get_geometry_srv = nh_.advertiseService("/gazebo/get_geometry", &GeometriesPlugin::getGeometrySrv, this);
+
 }
 
 GeometriesPlugin::~GeometriesPlugin()
@@ -21,8 +21,94 @@ void GeometriesPlugin::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
         return;
     }
 
+    nh_.reset(new ros::NodeHandle("geometries_plugin"));
+    nh_->setCallbackQueue(&queue_);
+
+    ros::param::param<std::vector<std::string>>("/gazebo/static_objects", static_objs_, {"INVALID"});
+    ros::param::param<std::string>("/robot_name", robot_name_, "j2s7s300");
+
+    base_frame_id_ = robot_name_ + "_link_base";
+    geometries_ns_ = "geometries";
+
+    marker_pub_ = nh_->advertise<visualization_msgs::Marker>("/geometries_markers", 10);
+    get_geometry_srv = nh_->advertiseService("/gazebo/get_geometry", &GeometriesPlugin::getGeometrySrv, this);
+
+    queue_thread_ = std::thread(std::bind(&GeometriesPlugin::queueThread, this));
+
     world_ = _world;
     ROS_INFO("%sGeometriesPlugin: Plugin loaded!", GREEN);
+}
+
+void GeometriesPlugin::queueThread()
+{
+    ros::Rate rate(5);
+    while (nh_->ok())
+    {
+        queue_.callAvailable(ros::WallDuration(0.05));
+        this->publishMarkers();
+        rate.sleep();
+    }
+}
+
+void GeometriesPlugin::publishMarkers()
+{
+    visualization_msgs::Marker marker;
+    int id = 0;
+    bool is_static = false;
+
+    std::vector<gazebo::physics::ModelPtr> models = world_->Models();
+    if (models.empty()) return;
+
+    for (size_t i = 0; i < models.size(); i++)
+    {
+        if (models[i]->GetName().find(robot_name_) != std::string::npos||
+            models[i]->GetName().find("ground_plane") != std::string::npos)
+            { continue; }
+
+        is_static = std::any_of(static_objs_.begin(), static_objs_.end(), [this, i, models](const std::string& str)
+            { return models[i]->GetName().find(str) != std::string::npos; });
+
+        // for (size_t j = 0; j < static_objs_.size(); j++)
+        // {
+        //     is_static = states_.name[i].find(static_objs_[j]) != std::string::npos;
+
+        //     if (is_static)
+        //     {
+        //         // std::cout << static_objs_[j] << " " << states_.name[i] << std::endl;
+        //         break;
+        //     }
+        // }
+
+        gazebo_geometries_plugin::geometry srv;
+        srv.request.model_name = models[i]->GetName();
+        if (this->getGeometrySrv(srv.request, srv.response))
+        {
+            for (size_t j = 0; j < srv.response.name.size(); j++)
+            {
+                if (!std::isfinite(srv.response.dimensions[j].x) || std::abs(srv.response.dimensions[j].x) <= 1e-5) continue;
+
+                // std::cout << srv.response.dimensions[j] << std::endl;
+                
+                marker.id = id;
+                marker.action = marker.ADD;
+                marker.type = marker.CUBE;
+                
+                if (is_static) { marker.color.r = marker.color.a = 1.0; marker.color.g = marker.color.b = 0.0; }
+                else { marker.color.g = marker.color.a = 1.0; marker.color.r = marker.color.b = 0.0; }
+
+                marker.pose = srv.response.pose[j];
+                marker.scale = srv.response.dimensions[j];
+
+                marker.header.frame_id = base_frame_id_;
+                marker.header.stamp = ros::Time::now();
+                marker.header.seq += 1;
+                marker.ns = geometries_ns_;
+
+                marker_pub_.publish(marker);
+                id++;
+            }
+        }
+    }
 }
 
 void GeometriesPlugin::getBBox(gazebo_geometries_plugin::geometry::Response &res, gazebo::physics::CollisionPtr geom)
@@ -35,6 +121,7 @@ void GeometriesPlugin::getBBox(gazebo_geometries_plugin::geometry::Response &res
     collision_box = geom->CollisionBoundingBox();
     min.x = collision_box.Min().X(); min.y = collision_box.Min().Y(); min.z = collision_box.Min().Z();
     max.x = collision_box.Max().X(); max.y = collision_box.Max().Y(); max.z = collision_box.Max().Z();
+    pose.position.x = collision_box.Center().X(); pose.position.y = collision_box.Center().Y(); pose.position.z = collision_box.Center().Z();
     
     // if the geometry has a box shape, an Object Oriented Bounding Box (OOBB) can be obtained directly, otherwise
     // an Axis Aligned Bounding Box (AABB) is obtained. The AABB is aligned with the world axes, whereas the
@@ -66,8 +153,6 @@ void GeometriesPlugin::getBBox(gazebo_geometries_plugin::geometry::Response &res
         pose.orientation.w = 1;
     }
 
-    pose.position.x = collision_box.Center().X(); pose.position.y = collision_box.Center().Y(); pose.position.z = collision_box.Center().Z();
-    
     res.min_bounds.push_back(min);
     res.max_bounds.push_back(max);
     res.dimensions.push_back(dimensions);
