8c163dc4a2f7782730a60c71e772edf2ed1bc24c
Moustafa <mustafa.motawei@gmail.com>
2021-02-13 19:46:13 +0000

    create timer class

---
+++ planner/include/planner/timer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <chrono>
+
+class Timer
+{
+public:
+    Timer();
+    ~Timer();
+
+    void start();
+    void stop();
+    void pause();
+    void reset();
+    int64_t elapsedMillis();
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_t_;
+    std::chrono::duration<long double> accum_t_;
+
+    bool stopped_;
+    bool paused_;
+};
