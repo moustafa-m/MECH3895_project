4f4eebf245dcc6da66cfac3733b0ad2a1d0bdb80
Moustafa <mustafa.motawei@gmail.com>
2021-03-19 17:07:00 +0000

    add grasp success rate plotting

--- planner/test/plot_plan_times.sh
+++ planner/test/plot_plan_times.sh
@@ -7,9 +7,9 @@ Usage()
 {
     printf "Usage: $0 [-h] -f [-a]\n"
     printf "Options:\n"
-    printf "h:\n\t Prints this message\n"
+    printf "h:\n\t Prints this message.\n"
     printf "f:\n\t File to plot.\n"
-    printf "a:\n\t Set this option if plotting averages\n"
+    printf "a:\n\t Set this option if plotting averages using point-to-point lines.\n"
 }
 
 if [ -z "$*" ]; then
@@ -17,7 +17,6 @@ if [ -z "$*" ]; then
     exit 0
 fi
 
-dir=""
 plot_avg=0
 title_prefix=""
 file=""
@@ -101,6 +100,32 @@ if (plot_avg > 0) {
     f(x) = a*x + b
     fit f(x) '${file}' using (\$8*100/\$9):4 via a, b
     plot \"${file}\" using (\$8*100/\$9):4 with points title('data'), f(x) title('best fit')
+}
+
+set term qt 3
+set title \"${title_prefix}Grasp Success Rate against Free Area Percentage\"
+set xlabel \"Free Area (%)\"
+set ylabel \"Grasp Success Rate\"
+
+if (plot_avg > 0) {
+    plot \"${file}\" using (\$8*100/\$9):3 with linespoints title('data')
+} else {
+    f(x) = a*x + b
+    fit f(x) '${file}' using (\$8*100/\$9):3 via a, b
+    plot \"${file}\" using (\$8*100/\$9):3 with points title('data'), f(x) title('best fit')
+}
+
+set term qt 4
+set title \"${title_prefix}Grasp Success Rate against Num Objs\"
+set xlabel \"Number of Objects\"
+set ylabel \"Grasp Success Rate\"
+
+if (plot_avg > 0) {
+    plot \"${file}\" using 7:3 with linespoints title('data')
+} else {
+    f(x) = a*x + b
+    fit f(x) '${file}' using 7:3 via a, b
+    plot \"${file}\" using 7:3 with points title('data'), f(x) title('best fit')
 }
 
 pause -1 \"Press Enter to exit\"" > gnuplot.in
